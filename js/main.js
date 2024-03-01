19eda8804695edf11ee3e23f926f84926f573892
Stiven Hernandez <casper.sa690@gmail.com>
2024-03-01 15:23:33 -0500

    class player added

---
+++ js/main.js
@@ -0,0 +1,10 @@
+class player {
+    constructor (id, nameplayer, accountnumber) {
+        this.id = id;
+        this.nameplayer = nameplayer;
+        this.accountnumber = accountnumber;
+        this.initialvalue = 100000;
+        this.accumulatedmoney = 0;       
+     
+    }
+}

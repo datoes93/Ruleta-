162bde4e109d5c2c6a80166084316f7700288cf5
Stiven Hernandez <casper.sa690@gmail.com>
2024-03-01 15:27:14 -0500

    class bet added and funtion addPlayer implemented

--- js/main.js
+++ js/main.js
@@ -8,3 +8,40 @@ class player {
      
     }
 }
+
+class Bets {
+    addPlayer(_objPlayer){ 
+
+        var tbl = document.getElementById('player-list-table').getElementsByTagName('tbody')[0];
+  
+         ///console.log(tbl.children());
+        var row = tbl.insertRow(-1);
+        var cellID = row.insertCell(-1);
+        var cellnameplayer = row.insertCell(-1);
+        var cellinitialvalue = row.insertCell(-1);
+        var cellaccount = row.insertCell(-1);
+        var cellbtndelete = row.insertCell(-1);
+ 
+        var textcellID = document.createTextNode(_objPlayer.id);      
+        var textnameplayer = document.createTextNode(_objPlayer.nameplayer);
+        let moneyformat = new Intl.NumberFormat("es-CO", {style: "currency", currency: "COP", maximumSignificantDigits: 3}).format(_objPlayer.initialvalue);
+        var textinitialvalue = document.createTextNode(moneyformat);
+        var textaccount = document.createTextNode(_objPlayer.accountnumber);
+    
+        var btncelldelete = document.createElement('input');
+        btncelldelete.setAttribute('type', 'button');
+        btncelldelete.setAttribute('class', 'btn btn-danger');
+        btncelldelete.setAttribute('id', _objPlayer.id);
+        btncelldelete.setAttribute('value', 'Delete');
+        btncelldelete.setAttribute('name', 'btndelete');
+        
+        
+ 
+        cellnameplayer.appendChild(textnameplayer);
+        cellinitialvalue.appendChild(textinitialvalue);
+        cellaccount.appendChild(textaccount);  
+        cellbtndelete.appendChild(btncelldelete);
+        cellID.appendChild(textcellID);
+     }
+ 
+}

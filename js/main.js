da11c49c078c79ac35327752b0fbe38eabc22607
Stiven Hernandez <casper.sa690@gmail.com>
2024-03-01 15:55:55 -0500

    finish bet and game

--- js/main.js
+++ js/main.js
@@ -96,6 +96,82 @@ class Bets {
       
   
       }
+
+      
+  FinishAll(element){
+    if(element.name === "FinishAll"){
+      let _objTemporal = Object.keys(objLista);
+      for (let i = 0; i < _objTemporal.length; i++) {
+        const property = _objTemporal[i];
+        objFinalize.push(objLista[property]);
+        delete objLista[property];
+        
+      }
+      const allplayer = document.getElementById('all-player');
+      const roulette = document.getElementById('roulette');
+      const report = document.getElementById('report');
+      const earningreport = document.getElementById('earningreport');
+
+      allplayer.style.display = 'none';
+      roulette.style.display = 'none';
+      report.style.display = 'block';
+      earningreport.style.display = 'block';
+      
+      //console.log(objFinalize);
+
+
+      var tablereport = document.getElementById('tablereport').getElementsByTagName('tbody')[0];
+      let objrecorrer = Object.keys(objFinalize);
+      for (let i = 0; i < objrecorrer.length; i++) {
+        const property = objrecorrer[i];
+        
+      
+      var row = tablereport.insertRow(-1);
+      var cellID = row.insertCell(-1);
+      var cellnameplayer = row.insertCell(-1);
+      var cellAmounttobePaid = row.insertCell(-1);
+      var cellTrend = row.insertCell(-1);    
+      let moneyformat = new Intl.NumberFormat("es-CO", {style: "currency", currency: "COP", maximumSignificantDigits: 3}).format(objFinalize[property].initialvalue);
+     
+      var textcellID = document.createTextNode(objFinalize[property].id);      
+      var textnameplayer = document.createTextNode(objFinalize[property].nameplayer);
+      var textAmounttobePaid = document.createTextNode(moneyformat);   
+      
+      var divtrend = document.createElement('div');
+      if(objFinalize[property].initialvalue > 100000){
+        divtrend.innerHTML = 
+        `<div  class="divtrend">           
+        <i class="divtendenciaup fas fa-arrow-up"></i>          
+        </div>`;
+
+      } else if (objFinalize[property].initialvalue === 100000) {
+        divtrend.innerHTML = 
+        `<div  class="divtrend">           
+        <i class="divtendencia fas fa-window-minimize"></i>          
+        </div>`;
+      }else {
+        divtrend.innerHTML = 
+        `<div  class="divtrend">           
+        <i class="divtendenciadown fas fa-arrow-down"></i>          
+        </div>`;
+      }
+
+      
+
+      cellID.appendChild(textcellID);
+      cellnameplayer.appendChild(textnameplayer);
+      cellAmounttobePaid.appendChild(textAmounttobePaid);
+     cellTrend.appendChild(divtrend);
+
+    }
+
+    let moneyProfit= new Intl.NumberFormat("es-CO", {style: "currency", currency: "COP", maximumSignificantDigits: 3}).format(Profits);
+    document.getElementById('profits').innerHTML = moneyProfit;
+    
+
+  }
+}
+
   
  
 }
